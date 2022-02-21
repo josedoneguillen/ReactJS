a55996383db365cd2f67f690d6c6c9fdcbaee2a4
Jose Doñe <josedoneguillen1992@hotmail.com>
2022-02-21 13:20:09 -0400

    Using inline style inside JSX tag

--- learning_react/src/index.js
+++ learning_react/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const name = 'Jose';
 const JSX = (
   <div>
-    <h1 className="main-title">Hello {name}!</h1>
+    <h1 className="main-title" style={{color: 'teal'}}>Hello {name}!</h1>
     <p className="main-paragraph">Have a nice day</p>
   </div>
 );
