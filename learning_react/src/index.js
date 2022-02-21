d8c8764368e465f816691016c4b2e7fd2e54b69c
Jose Doñe <josedoneguillen1992@hotmail.com>
2022-02-21 13:37:58 -0400

    Creating condition inside condition in a simple way

--- learning_react/src/index.js
+++ learning_react/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const name = 'Jose';
-const session = false;
+const session = true;
+const country = undefined;
 const JSX = (
   <div>{ session === true ?
     <div>
       <h1 className="main-title" style={{color: 'teal'}}>Hello {name}!</h1>
       <p className="main-paragraph">Have a nice day</p>
+      {country && <p>Tu eres de {country}</p>}
     </div>
     :
     <h1>You have not logged in</h1>
