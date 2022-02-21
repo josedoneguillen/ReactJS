4b49be913216cb7c8d7671b62a3f3521efe429cd
Jose Doñe <josedoneguillen1992@hotmail.com>
2022-02-21 13:16:55 -0400

    Creating JSX code in react and using multi tags and classname handling

--- learning_react/src/index.js
+++ learning_react/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World!</h1>,
+const name = 'Jose';
+const JSX = (
+  <div>
+    <h1 className="main-title">Hello {name}!</h1>
+    <p className="main-paragraph">Have a nice day</p>
+  </div>
+);
+
+ReactDOM.render(JSX,
   document.getElementById('root')
 );
