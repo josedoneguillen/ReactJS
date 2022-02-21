6caa96c23697af60073bea3a64be8d600f180718
Jose Doñe <josedoneguillen1992@hotmail.com>
2022-02-21 13:33:37 -0400

    Creating conditions inside JSX syntaxis using ternary operator

--- learning_react/src/index.js
+++ learning_react/src/index.js
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const name = 'Jose';
-const session = true;
+const session = false;
 const JSX = (
-  <div>
-    <h1 className="main-title" style={{color: 'teal'}}>Hello {name}!</h1>
-    <p className="main-paragraph">Have a nice day</p>
+  <div>{ session === true ?
+    <div>
+      <h1 className="main-title" style={{color: 'teal'}}>Hello {name}!</h1>
+      <p className="main-paragraph">Have a nice day</p>
+    </div>
+    :
+    <h1>You have not logged in</h1>
+    }  
   </div>
 );
 
-const validateSession = (session) => {
+/*const validateSession = (session) => {
   if(session === true){
       return JSX;
   } else {
     return <h1>You have not logged in</h1>
   }
-}
+}*/
 
-ReactDOM.render(validateSession(session),
+ReactDOM.render(JSX,
   document.getElementById('root')
 );
