654f10708abafe02856f8d2be829ec7d69d15b77
Jose Doñe <josedoneguillen1992@hotmail.com>
2022-02-23 15:30:33 -0400

    Creating react project with webpack and installig css scss support

---
+++ sass_react_webpack/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/index.scss';
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
---
+++ sass_react_webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+        ]
+      }
+      /*{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },*/
+    ]
+  }
+};
